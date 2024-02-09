8c9c17acd3ee8d876c194d2a911335da01304134
MadhuriKM <madhurikm07@gmail.com>
2024-02-09 12:15:15 +0530

    added express basics

---
+++ EXPRESS_BASICS/02-http-url.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const PORT = 4545
+
+// ref
+const app = express()
+
+// app.request(route,controller)
+
+//home path, index path
+app.get('/', function(req,res){
+    res.write(`<div>
+                    <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/login">Login</a>
+                </nav>
+                <h1>Home Page</h1>
+            </div>`)
+})
+
+// about page
+app.get('/about', function(req,res){
+    res.write(`<div>
+                    <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/login">Login</a>
+                </nav>
+                <h1>About Page</h1>
+            </div>`)
+})
+
+app.get('/*', function(req,res){
+    res.write(`<div>
+                    <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                </nav>
+                <h1>Page Not found</h1>
+            </div>`)
+})
+
+app.listen(PORT,() => {
+    console.log(`server is running @ http://localhost:${PORT}`)
+})
